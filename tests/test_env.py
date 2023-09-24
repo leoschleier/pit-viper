e3808109a5a674cc52663b0c257b29113aa5c72b
Leo Schleier <43878374+leoschleier@users.noreply.github.com>
2023-09-24 23:29:17 +0200

    Use temporary directory in unit test

--- tests/test_env.py
+++ tests/test_env.py
@@ -2,7 +2,9 @@
 import os
 from pathlib import Path
 
+import py
 import pytest
+
 from pyper import env
 
 _TEST_DOTENV_PATH = Path(__file__).parent / "data" / ".env"
@@ -16,7 +18,7 @@ def _prepare_env() -> None:
 
 
 def test_auto_env_without_overwrite() -> None:
-    """Test the auto_env function without overwrite.
+    """Test the `auto_env` function without overwrite.
 
     We expect that the value of the already existing environment
     variable `FOO` is not being overwritten with the value from the
@@ -31,7 +33,7 @@ def test_auto_env_without_overwrite() -> None:
 
 
 def test_auto_env_with_overwrite() -> None:
-    """Test the auto_env function with overwrite.
+    """Test the `auto_env` function with overwrite.
 
     We expect that the value of the already existing environment
     variable `FOO` is being overwritten with the value from the .env
@@ -45,15 +47,20 @@ def test_auto_env_with_overwrite() -> None:
     assert e["TEST_VAR"] == "test_value"
 
 
-def test_auto_env_with_non_existing_path() -> None:
-    """Test the auto_env function with a non-existing path.
+def test_auto_env_with_non_existing_path(tmpdir: py.path.local) -> None:
+    """Test the `auto_env` function with a non-existing path.
 
     We expect that the function still returns a dictionary of
     environment variables. Consequently, the dictionary must be non-
     empty. Moreover, the dictionary must not contain the variables
     provided in the test .env file.
+
+    Note that the `auto_env` function is supposed to not raise an error
+    even though the path a wrong file extension is being used.
     """
-    e = env.auto_env(path=Path("non-existing-path"))
+    # Use a temporary directory to ensure that the path does not exist.
+    non_existent_path = Path(tmpdir) / ".wrong"
+    e = env.auto_env(path=non_existent_path)
 
     assert e
     assert isinstance(e, dict)
